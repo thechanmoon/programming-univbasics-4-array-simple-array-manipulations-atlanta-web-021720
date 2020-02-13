7814f65f6b3b7f69260bf6da0e3a0366332f58a6
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-13 01:44:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,11 +3,11 @@ def using_push(array, string)
 end
 
 def using_unshift(array, string)
-  #array.unshift(string)
+  array.unshift(string)
 end
 
-def ()
-  #array.()
+def using_pop()
+  array.pop()
 end
 
 def ()
